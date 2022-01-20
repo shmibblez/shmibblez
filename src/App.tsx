9fe3a51b3a8c7eada32b4c9987f5ee1adefa02fd
shmibblez <shmibblez@gmail.com>
2022-01-20 18:15:45 -0500

    adding db access

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import { Box, Tabs, TabList, Tab, TabPanels, TabPanel, Flex } from '@chakra-ui/react';
 import Shmibblogo from './svgs/shmibblez_logo';
 import ShmibblezTextLogo from './svgs/shmibblez_text_logo';
 import { Home } from './pages/home';
 import { Store } from './pages/store';
 import { Cart } from './pages/cart';
+
+// TODO: move setup to redux on app start (async) and depending on state show loading in store and cart
+import * as Realm from "realm-web";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+// TODO
+export const APP_ID = "<Your App ID>";
+const graphqlUri = `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`
+// Local apps should use a local URI!
+// const graphqlUri = `https://us-east-1.aws.stitch.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`
+// const graphqlUri = `https://eu-west-1.aws.stitch.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`
+// const graphqlUri = `https://ap-southeast-1.aws.stitch.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: graphqlUri
+  }),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
