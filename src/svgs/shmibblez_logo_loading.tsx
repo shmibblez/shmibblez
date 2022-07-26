94daa06957313404e9c8d4aaae6cb5f5164819c4
shmibblez <shmibblez@gmail.com>
2022-07-25 23:51:22 -0400

    some styles and shmibblez loading animated svg

---
+++ src/svgs/shmibblez_logo_loading.tsx
@@ -0,0 +1,203 @@
+import * as React from "react";
+
+const ShmibblogoLoading = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="131.244"
+    height="130"
+    viewBox="-5 -5 131.244 130"
+    fill="none"
+    stroke="#FFF"
+    strokeLinejoin="round"
+    strokeLinecap="round"
+    {...props}
+  >
+    <g className="background" stroke="#FFFFFF11">
+      <path
+        className="o1"
+        d="M34.641 75v20l8.66-5v10l-17.32 10-8.66-5V65L0 55V45l34.641-20 8.66 5-25.98 15 17.32 10"
+      />
+      <path
+        className="o2"
+        d="m60.622 30-17.32-10v10l-8.661-5V5l8.66-5 34.641 20 17.32-10 8.661 5v40l-8.66 5V30l-17.32 10"
+      />
+      <path
+        className="o3"
+        d="m86.603 75 17.32-10-8.66-5 8.66-5 17.32 10v10l-34.64 20v20l-8.66 5L43.3 100V90l25.981 15V85"
+      />
+      <path className="c1" d="m43.301 80-17.32-10v40" />
+      <path className="c2" d="m51.962 35 17.32-10L34.641 5" />
+      <path className="c3" d="M86.603 65v20l34.64-20" />
+      <path className="i1" d="m25.98 50 17.321-10v30l8.66 5-17.32 10" />
+      <path className="i2" d="M51.962 25v20l8.66-5 25.98 15V35" />
+      <path className="i3" d="m95.263 70-17.32-10v10L51.961 85l17.32 10" />
+      <path className="c4" d="M34.641 45v20L0 45" />
+      <path className="c5" d="M103.923 15 69.282 35l17.32 10" />
+      <path className="c6" d="m60.622 90 17.32-10v40" />
+      <path className="h1" d="m43.301 70 8.66-5V55l8.66-5" />
+      <path className="h2" d="M60.622 40v10l8.66 5v10" />
+      <path className="h3" d="m77.942 70-8.66-5-8.66 5-8.66-5" />
+    </g>
+    <g className="animated">
+      <path
+        className="o1"
+        strokeDasharray={120}
+        d="M34.641 75v20l8.66-5v10l-17.32 10-8.66-5V65L0 55V45l34.641-20 8.66 5-25.98 15 17.32 10"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;240"
+          dur="4s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="o2"
+        strokeDasharray={120}
+        d="m60.622 30-17.32-10v10l-8.661-5V5l8.66-5 34.641 20 17.32-10 8.661 5v40l-8.66 5V30l-17.32 10"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;240"
+          dur="4s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="o3"
+        strokeDasharray={120}
+        d="m86.603 75 17.32-10-8.66-5 8.66-5 17.32 10v10l-34.64 20v20l-8.66 5L43.3 100V90l25.981 15V85"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;240"
+          dur="4s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path className="c1" strokeDasharray={30} d="m43.301 80-17.32-10v40">
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;60"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="c2"
+        strokeDasharray={30}
+        d="m51.962 35 17.32-10L34.641 5"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;60"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path className="c3" strokeDasharray={30} d="M86.603 65v20l34.64-20">
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;60"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="i1"
+        strokeDasharray={40}
+        d="m25.98 50 17.321-10v30l8.66 5-17.32 10"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;80"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="i2"
+        strokeDasharray={40}
+        d="M51.962 25v20l8.66-5 25.98 15V35"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;80"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="i3"
+        strokeDasharray={40}
+        d="m95.263 70-17.32-10v10L51.961 85l17.32 10"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;80"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path className="c4" strokeDasharray={30} d="M34.641 45v20L0 45">
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;60"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="c5"
+        strokeDasharray={30}
+        d="M103.923 15 69.282 35l17.32 10"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;60"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path className="c6" strokeDasharray={30} d="m60.622 90 17.32-10v40">
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;60"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path className="h1" strokeDasharray={15} d="m43.301 70 8.66-5V55l8.66-5">
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;30"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path className="h2" strokeDasharray={15} d="M60.622 40v10l8.66 5v10">
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;30"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+      <path
+        className="h3"
+        strokeDasharray={15}
+        d="m77.942 70-8.66-5-8.66 5-8.66-5"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          values="0;30"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </path>
+    </g>
+  </svg>
+);
+
+export default ShmibblogoLoading;
+export const shmibblogoLoadingUrlData = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='131.244' height='130' viewBox='-5 -5 131.244 130' fill='none' stroke='%23FFF' stroke-linejoin='round' stroke-linecap='round'%3E%3Cg class='background' stroke='%23FFFFFF11'%3E%3Cpath class='o1' d='M34.641 75v20l8.66-5v10l-17.32 10-8.66-5V65L0 55V45l34.641-20 8.66 5-25.98 15 17.32 10'/%3E%3Cpath class='o2' d='m60.622 30-17.32-10v10l-8.661-5V5l8.66-5 34.641 20 17.32-10 8.661 5v40l-8.66 5V30l-17.32 10'/%3E%3Cpath class='o3' d='m86.603 75 17.32-10-8.66-5 8.66-5 17.32 10v10l-34.64 20v20l-8.66 5L43.3 100V90l25.981 15V85'/%3E%3Cpath class='c1' d='m43.301 80-17.32-10v40'/%3E%3Cpath class='c2' d='m51.962 35 17.32-10L34.641 5'/%3E%3Cpath class='c3' d='M86.603 65v20l34.64-20'/%3E%3Cpath class='i1' d='m25.98 50 17.321-10v30l8.66 5-17.32 10'/%3E%3Cpath class='i2' d='M51.962 25v20l8.66-5 25.98 15V35'/%3E%3Cpath class='i3' d='m95.263 70-17.32-10v10L51.961 85l17.32 10'/%3E%3Cpath class='c4' d='M34.641 45v20L0 45'/%3E%3Cpath class='c5' d='M103.923 15 69.282 35l17.32 10'/%3E%3Cpath class='c6' d='m60.622 90 17.32-10v40'/%3E%3Cpath class='h1' d='m43.301 70 8.66-5V55l8.66-5'/%3E%3Cpath class='h2' d='M60.622 40v10l8.66 5v10'/%3E%3Cpath class='h3' d='m77.942 70-8.66-5-8.66 5-8.66-5'/%3E%3C/g%3E%3Cg class='animated'%3E%3Cpath class='o1' stroke-dasharray='120' d='M34.641 75v20l8.66-5v10l-17.32 10-8.66-5V65L0 55V45l34.641-20 8.66 5-25.98 15 17.32 10'%3E%3Canimate attributeName='stroke-dashoffset' values='0;240' dur='4s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='o2' stroke-dasharray='120' d='m60.622 30-17.32-10v10l-8.661-5V5l8.66-5 34.641 20 17.32-10 8.661 5v40l-8.66 5V30l-17.32 10'%3E%3Canimate attributeName='stroke-dashoffset' values='0;240' dur='4s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='o3' stroke-dasharray='120' d='m86.603 75 17.32-10-8.66-5 8.66-5 17.32 10v10l-34.64 20v20l-8.66 5L43.3 100V90l25.981 15V85'%3E%3Canimate attributeName='stroke-dashoffset' values='0;240' dur='4s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='c1' stroke-dasharray='30' d='m43.301 80-17.32-10v40'%3E%3Canimate attributeName='stroke-dashoffset' values='0;60' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='c2' stroke-dasharray='30' d='m51.962 35 17.32-10L34.641 5'%3E%3Canimate attributeName='stroke-dashoffset' values='0;60' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='c3' stroke-dasharray='30' d='M86.603 65v20l34.64-20'%3E%3Canimate attributeName='stroke-dashoffset' values='0;60' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='i1' stroke-dasharray='40' d='m25.98 50 17.321-10v30l8.66 5-17.32 10'%3E%3Canimate attributeName='stroke-dashoffset' values='0;80' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='i2' stroke-dasharray='40' d='M51.962 25v20l8.66-5 25.98 15V35'%3E%3Canimate attributeName='stroke-dashoffset' values='0;80' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='i3' stroke-dasharray='40' d='m95.263 70-17.32-10v10L51.961 85l17.32 10'%3E%3Canimate attributeName='stroke-dashoffset' values='0;80' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='c4' stroke-dasharray='30' d='M34.641 45v20L0 45'%3E%3Canimate attributeName='stroke-dashoffset' values='0;60' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='c5' stroke-dasharray='30' d='M103.923 15 69.282 35l17.32 10'%3E%3Canimate attributeName='stroke-dashoffset' values='0;60' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='c6' stroke-dasharray='30' d='m60.622 90 17.32-10v40'%3E%3Canimate attributeName='stroke-dashoffset' values='0;60' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='h1' stroke-dasharray='15' d='m43.301 70 8.66-5V55l8.66-5'%3E%3Canimate attributeName='stroke-dashoffset' values='0;30' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='h2' stroke-dasharray='15' d='M60.622 40v10l8.66 5v10'%3E%3Canimate attributeName='stroke-dashoffset' values='0;30' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3Cpath class='h3' stroke-dasharray='15' d='m77.942 70-8.66-5-8.66 5-8.66-5'%3E%3Canimate attributeName='stroke-dashoffset' values='0;30' dur='1s' repeatCount='indefinite'/%3E%3C/path%3E%3C/g%3E%3C/svg%3E`
